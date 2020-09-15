e18b6c4f0359e830026ee3f1a7a0b34c28de5878
Svan1233 <55134376+Svan1233@users.noreply.github.com>
2020-09-15 11:16:27 +0200

    Add files via upload

---
+++ svanTipuric1-1/svanTipuric1-1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace svanTipuric1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proceduralni x = new Proceduralni();
+            Objektni y = new Objektni();
+            Funkcijonalni z = new Funkcijonalni();
+
+            Console.WriteLine("Upisite vrijednost postotka za x objekt: ");
+            x.Postotak = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Upisite vrijednost postotka za y objekt: ");
+            y.Postotak = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Upisite vrijednost postotka za z objekt: ");
+            z.Postotak = Convert.ToInt32(Console.ReadLine());
+
+            if ((z.Postotak + y.Postotak + x.Postotak) != 100)
+            {
+                Console.WriteLine("Pogresan unos!");
+            }
+            else
+            {
+                if (x.Postotak > y.Postotak && x.Postotak > z.Postotak)
+                {
+                    Console.WriteLine("Najvise se koristi Proceduralni programski jezik.");
+                }
+                else if (y.Postotak > z.Postotak)
+                {
+                    Console.WriteLine("Najvise se koristi Objektni programski jezik.");
+                }
+                else 
+                {
+                    Console.WriteLine("Najvise se koristi Funkcijonalni programski jezik.");
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ svanTipuric1-1/svanTipuric1-1/ProgramskiJezik.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace svanTipuric1_1
+{
+    class ProgramskiJezik
+    {
+        double postotak;
+
+        public double Postotak { get => postotak; set => postotak = value; }
+
+        
+    }
+}
